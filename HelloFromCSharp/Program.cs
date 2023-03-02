7e0d2655aba77843004e68f01468ad46bc0bfe61
livingsneaky-art <ryanllamido@gmail.com>
2023-03-02 21:17:13 +0800

    update console

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Helo everybody");
+
+//Console.WriteLine("Please enter your name: ");
+
+/*
+Create a variable called name, string
+string name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
+*/
+
+Console.WriteLine("Please enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name: ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Hello " + firstName + " " + lastName);
